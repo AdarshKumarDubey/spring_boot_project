fab4fcd55cf349f6009e51bfb81059e6243c8192
AdarshKumarDubey <125072817+AdarshKumarDubey@users.noreply.github.com>
2026-05-13 10:54:12 +0530

    firtst comiit

---
+++ main (1).bash
@@ -0,0 +1,15 @@
+# 
+# Welcome to GDB Online.
+# GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+# C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+# Code, Compile, Run and Debug online from anywhere in world.
+# 
+# 
+sleep 100 &
+(echo"child process running";sleep 5)&
+echo"my pid is:$$"
+sleep 60 &
+echo"just forked background process with pid:$!"
+ps
+kill 1234
+sleep 5
